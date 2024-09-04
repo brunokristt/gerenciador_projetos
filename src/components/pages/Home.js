8a78f1705467d5d6310a194a412ac4f77d9ef659
Bruno Fabris <brunokristt@gmail.com>
2024-09-04 12:17:02 -0300

    fix: ajustes home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -6,6 +6,7 @@ function Home(){
     return (
         <section className={styles.home_container}>
             <h1>Bem-vindo ao <span>Gerenciador de Projetos</span></h1>
+            <p>Comece a gerenciar os seus projetos agora mesmo!</p>
             <LinkButton to="/newProject" text="Novo Projeto"></LinkButton>
             <img src={savings} alt='Gerenciador de Projetos'/>
         </section>
